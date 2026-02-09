240bba20b098dba07c5c7d93239f08f9a77447d5
amina9656 <aminaasharaf0123@gmail.com>
2026-02-09 16:12:17 +0530

    student_directory

---
+++ lib/service.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+Future<void> AddStudent(String name, String email, String course,BuildContext context) async {
+  await FirebaseFirestore.instance.collection("students").add({
+    "NAME": name,
+    "EMAIL": email,
+    "COURSE": course,
+  });
+  Navigator.pop(context);
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Student added Succesfully")));
+}
+Stream<QuerySnapshot> getStudents(){
+  return FirebaseFirestore.instance.collection("students").snapshots();
+}
+Future<void>deletestudent(String id,BuildContext context)async{
+  await FirebaseFirestore.instance.collection("students").doc(id).delete();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Deleted Succesfully")));
+}
